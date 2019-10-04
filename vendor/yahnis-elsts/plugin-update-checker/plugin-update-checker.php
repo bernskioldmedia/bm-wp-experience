a53677e013f68fc4f1d5261385fdb49528611ec6
Erik Bernskiold <erik@bernskioldmedia.com>
2019-10-04 23:35:26 +0200

    Ship PUC lib with plugin

---
+++ vendor/yahnis-elsts/plugin-update-checker/plugin-update-checker.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Update Checker Library 4.8
+ * http://w-shadow.com/
+ *
+ * Copyright 2019 Janis Elsts
+ * Released under the MIT license. See license.txt for details.
+ */
+
+require dirname(__FILE__) . '/Puc/v4p8/Autoloader.php';
+new Puc_v4p8_Autoloader();
+
+require dirname(__FILE__) . '/Puc/v4p8/Factory.php';
+require dirname(__FILE__) . '/Puc/v4/Factory.php';
