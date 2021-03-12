a4734b72aeea14266316796c79ff564631145528
Erik Bernskiold <erik@bernskioldmedia.com>
2021-03-12 12:26:46 +0100

    Show public staging environment

--- includes/class-environments.php
+++ includes/class-environments.php
@@ -19,7 +19,26 @@ namespace BernskioldMedia\WP\Experience;
 class Environments {
 
 	public static function init() {
-		add_action( 'admin_bar_menu', [ self::class, 'show_in_admin_bar' ], 40 );
+		add_filter( 'admin_bar_menu', [ self::class, 'show_in_admin_bar' ], 40 );
+		add_action( 'wp_footer', [ self::class, 'show_public_staging_notice' ] );
+	}
+
+	public static function show_public_staging_notice() {
+
+		if ( 'staging' !== wp_get_environment_type() ) {
+			return;
+		}
+
+		if ( false === apply_filters( 'bm_wpexp_environment_show_staging_public', true ) ) {
+			return;
+		}
+
+		if ( ! self::should_user_see() ) {
+			return;
+		}
+
+		include BM_WP_Experience::get_view_path( 'public/staging-message' );
+
 	}
 
 	/**
@@ -33,15 +52,13 @@ class Environments {
 			return;
 		}
 
-		$required_role = apply_filters( 'bm_wpexp_environment_role', 'manage_options' );
-
-		if ( ! current_user_can( $required_role ) ) {
+		if ( ! self::should_user_see() ) {
 			return;
 		}
 
 		$wp_admin_bar->add_node( [
 			'id'    => 'bm-environment',
-			'title' => self::get_admin_bar_environment_label(),
+			'title' => self::get_environment_label(),
 			'href'  => '#',
 			'meta'  => [
 				'class' => 'ab-environment-label environment--' . wp_get_environment_type(),
@@ -50,7 +67,13 @@ class Environments {
 
 	}
 
-	protected static function get_admin_bar_environment_label() {
+	protected static function should_user_see() {
+		$required_role = apply_filters( 'bm_wpexp_environment_role', 'manage_options' );
+
+		return current_user_can( $required_role );
+	}
+
+	protected static function get_environment_label() {
 
 		$environment = wp_get_environment_type();
 
