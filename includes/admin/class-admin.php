c1377a7a37f59ecd65b1e09a5819de2889c91cb4
Erik Bernskiold <erik@bernskioldmedia.com>
2021-07-16 12:36:30 +0200

    Add our help widget

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -24,6 +24,9 @@ class Admin {
 
 		// Remove the help tab.
 		add_filter( 'admin_head', [ self::class, 'remove_help_tab' ] );
+
+		// Add our help and support widget.
+		add_action( 'admin_footer', [ self::class, 'add_help_widget' ] );
 	}
 
 	/**
@@ -53,6 +56,37 @@ class Admin {
 	 */
 	public static function remove_help_tab() {
 		get_current_screen()->remove_help_tabs();
+	}
+
+	public static function add_help_widget() {
+		if ( false === apply_filters( 'bm_wpexp_show_admin_page_support', true ) ) {
+			return;
+		}
+
+		$user = get_user_by('ID', get_current_user_id());
+
+		?>
+		<script type="text/javascript" defer>
+			! function( e, t, n ) {
+				function a() {
+					var e = t.getElementsByTagName( 'script' )[0], n = t.createElement( 'script' );
+					n.type = 'text/javascript', n.async = ! 0, n.src = 'https://beacon-v2.helpscout.net', e.parentNode.insertBefore( n, e )
+				}
+
+				if ( e.Beacon = n = function( t, n, a ) {
+					e.Beacon.readyQueue.push( { method: t, options: n, data: a } )
+				}, n.readyQueue = [], 'complete' === t.readyState ) return a();
+				e.attachEvent ? e.attachEvent( 'onload', a ) : e.addEventListener( 'load', a, ! 1 )
+			}( window, document, window.Beacon || function() {
+			} );</script>
+		<script type="text/javascript" defer>
+			window.Beacon( 'init', '400d429a-e257-4a5d-bd60-97953b3a81c4' );
+			window.Beacon( 'identify', {
+				name: '<?php echo esc_js($user->first_name . ' '. $user->last_name); ?>',
+				email: '<?php echo esc_js($user->user_email); ?>'
+			} )
+		</script>
+		<?php
 	}
 
 }
