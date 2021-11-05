24cef75d9f2662caa79be093ee6cc3fdc01f6287
Erik Bernskiold <erik@bernskioldmedia.com>
2021-11-05 21:35:31 +0100

    Add about bm page

--- views/admin/about-bm.php
+++ views/admin/about-bm.php
@@ -8,11 +8,7 @@
 namespace BernskioldMedia\WP\Experience;
 
 ?>
-<div class="wrap">
-	<div class="about-bm-content">
-		<h1 class="about-bm-title">The Agency for a Digital World</h1>
-		<p>Bernskiold Media is a boutique, full-service digital agency. We work closely together with businesses for a targeted digital presence and beautiful user experiences to reach business critical goals.</p>
-		<p>The strength we bring is our broad experience. The ability to combine our expertise in business/management, communications, design and development is what sets us apart from other digital agencies.</p>
-		<p>Whether you want to build a complete website, create a mobile app, freshen up your content and product offering, run a digital campaign and build a campaign website, or something entirely different, we will go on the journey with you.</p>
-	</div>
+<div class="wrap" style="margin: -8px 0 -20px -20px;">
+	<iframe src="<?php echo esc_url( _x( 'https://bernskioldmedia.com/en/cc-embed/about/', 'bm website about url',
+		'bm-wp-experience' ) ); ?>" frameborder="0" style="width: 100%; height: 100vh;"></iframe>
 </div>
