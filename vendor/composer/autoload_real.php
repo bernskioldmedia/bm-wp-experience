b9ef6957754b15bec6be165e1cac68cf3a657fbb
Erik Bernskiold <erik@bernskioldmedia.com>
2021-11-05 13:47:15 +0100

    Add devtools

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitc6947907303abaaf7a0a6b488c10bb8a
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitc6947907303abaaf7a0a6b488c10bb8a', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitc6947907303abaaf7a0a6b488c10bb8a', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
