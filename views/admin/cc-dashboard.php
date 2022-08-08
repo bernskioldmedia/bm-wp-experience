fe50f9806fa28839de67f4bc9123a7caaa1a41e5
Erik Bernskiold <erik@bernskioldmedia.com>
2022-08-08 22:52:35 +0200

    Add "My Website" Dashboard (#76)
    * Add website dashboard
    
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>
    
    * Docs
    
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>
    
    * Add translations
    
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

---
+++ views/admin/cc-dashboard.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Admin Page View: About Bernskiold Media
+ *
+ * @package BernskioldMedia\WP\Experience
+ */
+
+namespace BernskioldMedia\WP\Experience;
+
+$website_id = defined( 'BM_WP_WEBSITE_UUID' ) ? BM_WP_WEBSITE_UUID : null;
+
+if ( ! $website_id ) {
+	return;
+}
+
+?>
+<div class="wrap" style="margin: -8px 0 0 -20px;">
+	<iframe src="https://connect.bernskioldmedia.test/embed/website-dashboard/<?php echo esc_attr( $website_id ); ?>" frameborder="0" style="width: 100%; height: 100vh;"></iframe>
+</div>
+<style>
+	#wpfooter {display: none;}
+
+	#wpbody-content {padding-bottom: 0;}
+</style>
