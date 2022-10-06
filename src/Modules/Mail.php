a8d7b8d7c7d24d73465f2cefe9cde33c1a2255cd
Erik Bernskiold <erik@bernskioldmedia.com>
2022-10-06 19:31:51 +0200

    Add support for sending email via postal
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- src/Modules/Mail.php
+++ src/Modules/Mail.php
@@ -3,10 +3,15 @@
 namespace BernskioldMedia\WP\Experience\Modules;
 
 use PHPMailer\PHPMailer\PHPMailer;
+use Postal\Client;
+use Postal\Error;
+use Postal\SendMessage;
+use WP_Error;
 
 class Mail extends Module {
 
 	public static function hooks(): void {
+		add_filter( 'pre_wp_mail', [ self::class, 'send_mail_via_postal' ], 10, 2 );
 		add_action( 'phpmailer_init', [ self::class, 'send_mail_via_smtp' ] );
 		add_action( 'admin_notices', [ self::class, 'warn_improper_smtp_configuration' ] );
 	}
@@ -89,6 +94,104 @@ class Mail extends Module {
 
 	protected static function are_all_configs_set(): bool {
 		return ! empty( self::get_smtp_host() ) && ! empty( self::get_smtp_username() ) && ! empty( self::get_smtp_password() ) && is_int( self::get_smtp_port() );
+	}
+
+	protected static function get_postal_domain(): ?string {
+		return defined( 'BM_WP_POSTAL_DOMAIN' ) ? BM_WP_POSTAL_DOMAIN : 'https://postal.oderland.com';
+	}
+
+	protected static function get_postal_api_key(): ?string {
+		return defined( 'BM_WP_POSTAL_API_KEY' ) ? BM_WP_POSTAL_API_KEY : null;
+	}
+
+	protected static function should_send_via_postal(): bool {
+		if ( 'production' !== wp_get_environment_type() ) {
+			return self::should_send_postal_outside_production();
+		}
+
+		return self::should_send_postal_in_production();
+	}
+
+	protected static function should_send_postal_outside_production(): bool {
+		return defined( 'BM_WP_POSTAL_OUTSIDE_PRODUCTION' ) && true === BM_WP_POSTAL_OUTSIDE_PRODUCTION;
+	}
+
+	protected static function should_send_postal_in_production(): bool {
+		return defined( 'BM_WP_POSTAL_ENABLED' ) && true === BM_WP_POSTAL_ENABLED;
+	}
+
+	public static function send_mail_via_postal( $null, $atts ): ?bool {
+
+		if ( ! self::should_send_via_postal() ) {
+			return null; // Null return means to use wp_mail default.
+		}
+
+		if ( null === self::get_postal_api_key() ) {
+			return null; // Process by normal wp_mail.
+		}
+
+		$client  = new Client( self::get_postal_domain(), self::get_postal_api_key() );
+		$message = new SendMessage( $client );
+
+		if ( is_array( $atts['headers'] ) ) {
+			$headers = $atts['headers'];
+		} else {
+			$headers = explode( "\n", str_replace( "\r\n", "\n", $atts['headers'] ) );
+		}
+
+		if ( ! empty( $headers ) ) {
+			foreach ( $headers as $header ) {
+				[ $name, $content ] = explode( ':', trim( $header ), 2 );
+				$message->header( $name, $content );
+			}
+		}
+
+		if ( is_array( $atts['to'] ) ) {
+			foreach ( $atts['to'] as $email ) {
+				$message->to( $email );
+			}
+		} else {
+			$message->to( $atts['to'] );
+		}
+
+		$message->subject( $atts['subject'] );
+		$message->tag( get_bloginfo( 'name' ) );
+
+		$from_name  = apply_filters( 'wp_mail_from_name', get_bloginfo( 'name' ) );
+		$from_email = apply_filters( 'wp_mail_from', null );
+
+		if ( ! $from_email ) {
+			$sitename   = wp_parse_url( network_home_url(), PHP_URL_HOST );
+			$from_email = 'wordpress@';
+
+			if ( null !== $sitename ) {
+				if ( 'www.' === substr( $sitename, 0, 4 ) ) {
+					$sitename = substr( $sitename, 4 );
+				}
+
+				$from_email .= $sitename;
+			}
+		}
+
+		$message->from( "$from_name <$from_email>" );
+
+		$content_type = apply_filters( 'wp_mail_content_type', 'text/plain' );
+
+		if ( $content_type === 'text/plain' ) {
+			$message->plainBody( $atts['message'] );
+		} else {
+			$message->htmlBody( $atts['message'] );
+		}
+
+		try {
+			$result = $message->send();
+
+			return count( $result->recipients() ) >= 1;
+		} catch ( Error $error ) {
+			do_action( 'wp_mail_failed', new WP_Error( 'wp_mail_failed', $error->getMessage(), $message ) );
+
+			return false;
+		}
 	}
 
 }
