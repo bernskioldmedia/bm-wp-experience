317eb75f6c337fd44b9ae38f144f7b62c165ec5b
Ann Lickander <ann@bernskioldmedia.com>
2023-08-09 15:37:26 +0200

    fix issue where we get an warning about wp being undefined (#106)

--- assets/scripts/admin-customize-support.js
+++ assets/scripts/admin-customize-support.js
@@ -1,4 +1,4 @@
-if( ! bmexp_supports.allow_autocomplete ){
+if( ! bmexp_supports.allow_autocomplete && wp !== undefined && wp.hooks !== undefined && wp.hooks.removeFilter !== undefined ){
     const remove_user_autocomplete = wp.hooks.removeFilter(
         'editor.Autocomplete.completers',
         'editor/autocompleters/set-default-completers'
